707fecdf70039fa8ca04adfd51ddd4d636a85131
khiranouchi <khiranouchi.lbsa@gmail.com>
2018-12-03 01:58:46 +0900

    Make all lyrics unable edit when song complete-flag is true

--- resources/views/song/lyrics_boxes.blade.php
+++ resources/views/song/lyrics_boxes.blade.php
@@ -3,7 +3,9 @@
     <!-- lyrics-old -->
     <div id="z_box_line_old_{{ $lyrics_box->id }}" class="x-lyrics-old x-row-margin-reset row">
         <div class="x-lyrics-text"
+             @if (!$song->is_complete)
              onclick="SwitchInputMode(this, '{{ route('lyrics_boxs.update', ['song' => $song, 'lyrics_box' => $lyrics_box]) }}', 'lyrics_old', false)"
+             @endif
         @if ($lyrics_box->lyrics_old === "")
         >(empty)</div>
         @else
@@ -11,24 +13,32 @@
         @endif
 
         <!-- Delete button -->
+        @if (!$song->is_complete)
         <div class="x-lyrics-box-delete"
              onclick="DeleteBox(this, '{{ route('lyrics_boxs.destroy', ['song' => $song, 'lyrics_box' => $lyrics_box]) }}')"
         >[[-]]</div>
+        @endif
 
         <!-- Insert before button -->
+        @if (!$song->is_complete)
         <div class="x-lyrics-box-insert-before"
              onclick="InsertBox('z_box_{{ $lyrics_box->id }}', '{{ route('lyrics_boxs.store', ['song' => $song]) }}', '{{ $lyrics_box->box_idx }}', true)"
         >[[^]]</div>
+        @endif
 
         <!-- Insert button -->
+        @if (!$song->is_complete)
         <div class="x-lyrics-box-insert"
              onclick="InsertBox('z_box_{{ $lyrics_box->id }}', '{{ route('lyrics_boxs.store', ['song' => $song]) }}', '{{ $lyrics_box->box_idx }}')"
         >[[v]]</div>
+        @endif
 
         <!-- Insert button of box-line -->
+        @if (!$song->is_complete)
         <div class="x-lyrics-line-insert"
              onclick="InsertBoxLine('z_box_line_old_{{ $lyrics_box->id }}', '{{ route('lyrics_box_lines.store', ['song' => $song, 'lyrics_box' => $lyrics_box]) }}', '-1')"
         >[+]</div>
+        @endif
     </div>
 
     <!-- lyrics-new -->

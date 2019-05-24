1451561cb2b6513baab190fa0d1b81ee4b671e97
khiranouchi <khiranouchi.lbsa@gmail.com>
2019-05-24 17:31:58 +0900

    Prepare for deploy: Improve setup.sh to display query insert result

--- setup_code.py
+++ setup_code.py
@@ -9,6 +9,9 @@ target_list = sys.argv[1:]
 # generate query from current datetime
 query = '?' + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
+# strings of tags which query is not inserted
+str_tags_not_inserted = ''
+
 # process all files recursively
 rootdir = 'resources/views'
 for pathname, dirnames, filenames in os.walk(rootdir):
@@ -32,12 +35,16 @@ for pathname, dirnames, filenames in os.walk(rootdir):
 
             # search '***.css'/'***.js' and insert query
             insert_idx_list = []
-            for m in re.finditer(r'css|js', str_tag):
+            for m in re.finditer(r'\.css|\.js', str_tag):
                 idx_end = m.end()
+                flg_fnd = False
                 for target in target_list:
                     if str_tag[idx_end-len(target):idx_end] == target:
                         insert_idx_list.append(idx_end)
+                        flg_fnd = True
                         break
+                if not flg_fnd:
+                    str_tags_not_inserted += '  ' + str_tag + '\n'
             for insert_idx in reversed(insert_idx_list):  # nessecery to reverse list because this process modify str_tag itself
                 if str_tag[insert_idx] == '?':  # if query already exists
                     str_tag = str_tag[:insert_idx] + query + str_tag[insert_idx+15:]  # 15=len('?20190101235959')
@@ -52,3 +59,7 @@ for pathname, dirnames, filenames in os.walk(rootdir):
         # write
         with open(file_path, 'w') as f:
             f.write(res)
+
+# print string of tags which query was not inserted
+print('Tags which query was not inserted:')
+print(str_tags_not_inserted[:-1])
